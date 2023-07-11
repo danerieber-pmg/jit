e29690faaacb8faafc43104894be1f4c9ae6b43c
Dane Rieber <dane.rieber@pmg.com>
2023-07-11 16:21:13 -0500

    Send CSV file directly to S3 bucket

--- tables/issues.js
+++ tables/issues.js
@@ -1,11 +1,14 @@
 import { jira, fid } from './init.js';
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 dayjs().format();
-import { createObjectCsvWriter } from "csv-writer";
+import { createObjectCsvWriter } from 'csv-writer';
+import AWS from 'aws-sdk';
+import fs from 'fs';
 
 
 
-const CSV_PATH = './csv/issues.csv';
+const CSV_PATH = 'jit_issues.csv';
+const DAY_FORMAT = 'YYYY-MM-DD';
 
 
 
@@ -17,10 +20,10 @@ async function getAllIssues() {
             fields: [fid['Created'], fid['Resolved'], fid['Status'], fid['Assignee'], fid['Responsible Team'], fid['Story Points']]
         })
         .select(r => r.issues)
-        .map(({id, fields}) => ({
+        .map(({ id, fields }) => ({
             id,
-            created: fields[fid['Created']],
-            resolved: fields[fid['Resolved']],
+            created: dayjs(fields[fid['Created']]).format(DAY_FORMAT),
+            resolved: dayjs(fields[fid['Resolved']]).format(DAY_FORMAT),
             status: fields[fid['Status']].name,
             assignee: fields[fid['Assignee']]?.displayName,
             team: fields[fid['Responsible Team']]?.value,
@@ -32,7 +35,7 @@ const results = await getAllIssues();
 
 
 
-async function writeToCsv(results) { 
+async function writeToCsv(results) {
     await createObjectCsvWriter({
         path: CSV_PATH,
         header: Object.keys(results[0]).map(k => ({ id: k, title: k }))
@@ -40,3 +43,25 @@ async function writeToCsv(results) {
         .writeRecords(results);
 }
 await writeToCsv(results);
+
+
+
+function sendToS3(csvPath) {
+    const s3 = new AWS.S3();
+    const fileStream = fs.createReadStream(csvPath);
+    fileStream.on('error', console.error);
+    const uploadParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: csvPath,
+        Body: fileStream
+    };
+    s3.upload(uploadParams, (err, data) => {
+        if (err) {
+            console.error(`S3 upload error: ${err}`);
+        }
+        if (data) {
+            console.log(`Uploaded to ${data.Location}`);
+        }
+    });
+}
+sendToS3(CSV_PATH);
